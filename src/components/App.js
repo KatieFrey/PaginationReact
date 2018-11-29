1bb1984e1972ae6e21eae943dfcd7f91bfa9c981
Katharine Francis <katie-francis@hotmail.com>
2018-11-29 12:17:10 -0500

    Added Event Handlers

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import PageSearch from "./PageSearch";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ margin: "10px" }}>
-      <PageSearch />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ margin: "10px" }}>
+        <PageSearch onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/PageSearch.js
+++ src/components/PageSearch.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 
 class PageSearch extends Component {
-  onInputChange() {}
+  state = {
+    term: ""
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => {
+                this.setState({ term: e.target.value });
+              }}
+            />
           </div>
         </form>
       </div>
